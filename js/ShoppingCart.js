8e1a42cdb6eefb0e474a02f7b7c2fc95832523d9
Kirthika-github <siva.kirthi0@gmail.com>
2022-02-10 13:48:01 +1100

    updated cart table and item quantity

--- js/ShoppingCart.js
+++ js/ShoppingCart.js
@@ -1,3 +1,4 @@
+// creates cart Html
 const createCartHtml = (id, name, price, quantity, itemTotal) => {
     const cartHtml =`<tr>
         <th scope="row">${name}</th>
@@ -14,30 +15,42 @@ const createCartHtml = (id, name, price, quantity, itemTotal) => {
   return cartHtml;
 }
 
+//Finds the total cost of the item with quantity
 const findItemTotal = (price, quantity) => {
     return price*quantity;
 }
 
-
+//Shopping cart class 
 class ShoppingCart{
     constructor(itemId = 0){
         this.cart = [];
         this.itemId = itemId;
     }
 
+    //adds a new item to the cart list array
     addToCart(name, price, quantity){
         this.itemId++;
-
         const newItem = {
             id: this.itemId,
             name: name,
             price: price,
             quantity: quantity
         };
-
         this.cart.push(newItem);
     }
 
+    //calculates thge total quatity of an item purchased
+    calculateQuantity(itemName){
+      let count = 0;
+      for (let i = 0; i < this.cart.length; i++) {
+        if (String(this.cart[i].name) == String(itemName)) {
+          count = count + Number(this.cart[i].quantity);
+        }
+      }
+      return count;
+    }
+
+    //gets an item object with a given id
     getItemById(itemId){
         let foundItem;
         for(let i=0; i<this.cart.length; i++){
@@ -50,12 +63,11 @@ class ShoppingCart{
       }
       
 
-
-    render(){
-        
-        let cartHtmlList = [];
-
-        const cartTitle = `<table class="table">
+  //updates all the current item details in the cart
+  render() {
+    let cartHtmlList = [];
+    //Form the cart table header
+    const cartTitle = `<table class="table">
         <thead>
           <tr>
             <th scope="col">Product Name</th>
@@ -64,48 +76,52 @@ class ShoppingCart{
             <th scope="col">Total</th>
           </tr>
         </thead>
-        <tbody>`;
+        <tbody>
+        <tr>
+               <th scope="row"></th>
+               <td></td>
+               <td></td>
+               <td></td>
+             </tr>`;
 
-        cartHtmlList.push(cartTitle);
-        
-        if(this.cart.length==0){
-            const cartHtml =`<tr>
+    cartHtmlList.push(cartTitle);
+    //checks for number of items added and renders accordingly
+    if (this.cart.length == 0) {
+      const cartHtml = `<tr>
             <td>Your cart is empty</td>
           </tr>`
-          cartHtmlList.push(cartHtml);
-        }else{
-            let total = 0;
-            for(let i=0;i<this.cart.length;i++){
-                const item = this.cart[i];
-              
-                const itemTotal = findItemTotal(Number(String(item.price).replace("$","").trim()),Number(item.quantity))
-                total=total+itemTotal;
-                const cartHtml = createCartHtml(item.id, item.name, item.price, item.quantity, itemTotal);
-                cartHtmlList.push(cartHtml);
-               }
-
-               const totalHtml = `<tr>
+      cartHtmlList.push(cartHtml);
+    } else {
+      //calculates and renders the total amount purchased
+      let total = 0;
+      for (let i = 0; i < this.cart.length; i++) {
+        const item = this.cart[i];
+        const itemTotal = findItemTotal(Number(String(item.price).replace("$", "").trim()), Number(item.quantity))
+        total = total + itemTotal;
+        const cartHtml = createCartHtml(item.id, item.name, item.price, item.quantity, itemTotal);
+        cartHtmlList.push(cartHtml);
+      }
+      const totalHtml = `<tr>
                <th scope="row"></th>
                <td></td>
                <td><b>Total</b></td>
                <td>$ ${total}</td>
              </tr>`;
-             cartHtmlList.push(totalHtml);
-      
-
-        }
-
-
-         const cartFooter =` </tbody>
+      cartHtmlList.push(totalHtml);
+    }
+    //adds the final html tags to complete the table
+    const cartFooter = ` </tbody>
          </table>`;
-         cartHtmlList.push(cartFooter);
+    cartHtmlList.push(cartFooter);
 
 
-        const cartHtmlList_updated = cartHtmlList.join("\n");
-       
-           const cartList = document.querySelector("#cart-list");
-           cartList.innerHTML = cartHtmlList_updated;
-         }
+    //Keeps every html element in a new line
+    const cartHtmlList_updated = cartHtmlList.join("\n");
+
+    //updates the cart with item details
+    const cartList = document.querySelector("#cart-list");
+    cartList.innerHTML = cartHtmlList_updated;
+  }
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,35 +1,45 @@
+//creates an instance of the ShoppingCart class
 const shoppingCart = new ShoppingCart(0);
+//makes sure the cart is displayed with correct details
 shoppingCart.render();
 
+//NOTE: handles just first item from shopping webpage for now. 
+//will be implementing this functionality to all the other items using class list 
 const addItem = document.querySelector("#addHeadphoneItem1");
-
 const itemName = document.querySelector("#headphoneItem1Name");
 const itemPrice = document.querySelector("#headphoneItem1Price");
 const itemQuantity = document.querySelector("#headphoneItem1Quantity");
 
+//Listens for a button click event ADD TO CART
 addItem.addEventListener("click", (event) => {
-    
-shoppingCart.addToCart(itemName.innerHTML, itemPrice.innerHTML, itemQuantity.value);
-
+    //adds a new item to the cart
+    shoppingCart.addToCart(itemName.innerHTML, itemPrice.innerHTML, itemQuantity.value);
+    //updates the quantity selected for each item based on their unique name
+    const quantity = document.querySelector("#quantityAdded");
+    let count = shoppingCart.calculateQuantity(itemName.innerHTML)
+    quantity.innerHTML = "Quantity in cart: " + String(count);
+    //renders the updated cart
+    shoppingCart.render();
 
 })
 
-
+//renders the updated cart
 const viewCart = document.querySelector("#viewCartButton");
-
 viewCart.addEventListener("click", (event) => {
-
     shoppingCart.render();
-    
-    
-    })
+})
 
-    const deleteItem = document.querySelector("#cart-list");
-    deleteItem.addEventListener("click",(event) => {
-        const itemToDelete=shoppingCart.getItemById(event.target.id);
-        shoppingCart.cart.splice(shoppingCart.cart.indexOf(itemToDelete))
-        shoppingCart.render();
-     
-    })
+const deleteItem = document.querySelector("#cart-list");
+//listens for click event from the cart list
+deleteItem.addEventListener("click", (event) => {
+    //deletes the selected item and updates the quantity added displayed on each item
+    const itemToDelete = shoppingCart.getItemById(event.target.id);
+    shoppingCart.cart.splice(shoppingCart.cart.indexOf(itemToDelete), 1)
+    shoppingCart.render();
+    const quantity = document.querySelector("#quantityAdded");
+    let count = shoppingCart.calculateQuantity(itemName.innerHTML)
+    quantity.innerHTML = "Quantity in cart: " + String(count);
+
+})
 
 
